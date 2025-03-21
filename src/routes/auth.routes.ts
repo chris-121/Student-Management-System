979a754d1cd6c1df34bc1259141489ce8c921191
chris <chrisjacob876@gmail.com>
2025-03-21 21:52:28 +0530

    request method change

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,7 +3,7 @@ import * as authController from "../controller/auth.controller";
 
 const router = Router();
 
-router.route("/login").get(authController.login);
+router.route("/login").post(authController.login);
 router.route("/logout").get(authController.logout);
 
 export default router;
